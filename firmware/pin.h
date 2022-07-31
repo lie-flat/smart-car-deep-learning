b42d9c515fc6e5d1714296a1a0ccbf31db6bd9b5
kxxt <rsworktech@outlook.com>
2022-07-31 12:24:08 +0800

    feat: header for pin definitions

---
+++ firmware/pin.h
@@ -0,0 +1,15 @@
+// servo
+const int servo = 13;
+const int servoChan = 0;
+
+// LED
+const int led1 = 32;  // Green LED
+const int led2 = 33;  // Red LED
+
+// Keys
+const int key1 = 35;
+const int key2 = 34;
+
+// Motor
+const int motorA = 26;
+const int motorB = 27;
