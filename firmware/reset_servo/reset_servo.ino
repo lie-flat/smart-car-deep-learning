2de1828b27624b8e0bf6009be6813e094b6fc0b6
kxxt <rsworktech@outlook.com>
2022-08-01 21:02:12 +0800

    refact: migrate to new api

--- firmware/reset_servo/reset_servo.ino
+++ firmware/reset_servo/reset_servo.ino
@@ -1,17 +1,10 @@
-const int servoPin = 13;
-const int freq = 50;
-const int resolution = 8;
-const int servoChannel = 0;
+#include <lie-flat.h>
 
 void setup() {
-  Serial.begin(115200);
-  ledcSetup(servoChannel, freq, resolution);
-  ledcAttachPin(servoPin, servoChannel);
-  ledcWrite(servoChannel, 20); // 7 -> 32
+  init_servo(servo);
+  set_servo(7.5);  // mid pos
+  servo_start_pwm();
   Serial.println("Successfully reset the servo position.");
 }
 
-
-void loop(){
-  
-}
+void loop() {}
