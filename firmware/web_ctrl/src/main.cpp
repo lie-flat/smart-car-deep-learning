fe11f1261dbc87f2129b76061b0776a7917d88aa
kxxt <rsworktech@outlook.com>
2022-08-02 09:01:22 +0800

    feat: read MPU6050

--- firmware/web_ctrl/src/main.cpp
+++ firmware/web_ctrl/src/main.cpp
@@ -2,10 +2,14 @@
 #include <ESPAsyncWebServer.h>
 #include <WiFi.h>
 #include <config.h>
-#include <lie-flat.h>
 #include <constants.h>
+#include <lie-flat.h>
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
 
 AsyncWebServer server(80);
+Adafruit_MPU6050 mpu;
 
 void notFound(AsyncWebServerRequest* request) {
   request->send(404, "text/plain", "Not found");
@@ -26,18 +30,32 @@ void setup() {
   servo_start_pwm();
   // Serial
   Serial.begin(115200);
-  Serial.println("Application started! :)");
+  while (!Serial) {
+    // Wait until serial connects
+    delay(10);
+  }
+  Serial.println("INFO: Application started! :)");
   // Connect to WiFi
   WiFi.mode(WIFI_STA);  // station mode
   WiFi.begin(ssid, password);
-  if (WiFi.waitForConnectResult() != WL_CONNECTED) {
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
     Serial.println(
-        "Failed to connect to WiFi!\nPlease check your ssid and password in "
+        "ERROR: Failed to connect to WiFi!\nPlease check your ssid and password in "
         "include/config.h!");
-    return;
+    delay(500);
   }
-  Serial.print("Connected to WiFi! My IP Address is ");
+  Serial.print("INFO: Connected to WiFi! My IP Address is ");
   Serial.println(WiFi.localIP());
+  // MPU6050
+  Wire.setPins(16,17);
+  while (!mpu.begin()) {
+    Serial.println("ERROR: could not connect to a valid MPU6050 sensor!");
+    delay(500);
+  }
+  Serial.println("INFO: MPU6050 sensor ready!");
+  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+  mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+  mpu.setFilterBandwidth(MPU6050_BAND_5_HZ);
   // Web server
   server.on("/", HTTP_GET, [](AsyncWebServerRequest* request) {
     request->send(200, "text/plain", "Hello, world");
@@ -50,6 +68,33 @@ void setup() {
     if (request->hasParam(MOTOR_B_PARAM, true))
       set_b(parse_param(request, MOTOR_B_PARAM));
     request->send(200, "text/plain", "OK");
+  });
+  server.on("/ping", HTTP_GET, [](AsyncWebServerRequest* request) {
+    request->send(200, "text/plain", "Pong");
+  });
+  server.on("/read", HTTP_GET, [](AsyncWebServerRequest* request) {
+    sensors_event_t acc, gyro, temp;
+    // acc: m/s^2
+    // gyro: rad/s
+    // temp: Celsius
+    mpu.getEvent(&acc, &gyro, &temp);
+    request->send(200, "application/json", "{\"acceleration\":{"
+                                             "\"x\":" +
+                                             String(acc.acceleration.x) +
+                                             ",\"y\":" +
+                                             String(acc.acceleration.y) +
+                                             ",\"z\":" +
+                                             String(acc.acceleration.z) +
+                                             "},\"gyro\":{"
+                                             "\"x\":" +
+                                             String(gyro.gyro.x) +
+                                             ",\"y\":" +
+                                             String(gyro.gyro.y) +
+                                             ",\"z\":" +
+                                             String(gyro.gyro.z) +
+                                             "},\"temp\":" +
+                                             String(temp.temperature) +
+                                             "}");
   });
   server.onNotFound(notFound);
   server.begin();
