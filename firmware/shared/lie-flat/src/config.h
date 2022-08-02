fea7c0cae8901c9057fa76e1b12a3bbeaefc43d9
kxxt <rsworktech@outlook.com>
2022-08-02 10:07:13 +0800

    feat: conditional compilation for Wifi constants

--- firmware/shared/lie-flat/src/config.h
+++ firmware/shared/lie-flat/src/config.h
@@ -1,5 +1,9 @@
+#ifdef LIE_FLAT_WIFI_STA
 const char* SSID = "Your SSID here";
 const char* PASSWORD = "Your password here";
-
+#elif defined LIE_FLAT_WIFI_AP
 const char* AP_SSID = "lie-flat";
 const char* AP_PASSWORD = "flat-lie";
+#else
+#error "LIE_FLAT_WIFI_STA or LIE_FLAT_WIFI_AP must be defined!"
+#endif
