3b1108f3bfba1f7117f16e907bfd3b5b5176b44b
kxxt <rsworktech@outlook.com>
2022-08-06 18:04:32 +0800

    feat(client/main): use logging

--- client/main.py
+++ client/main.py
@@ -1,26 +1,31 @@
 from controller import connect_to_board, read_sensors, control
 from camera import CameraReader
+import logging
+from logging import getLogger
 import cv2
 import random
 
 def main():
+    log = getLogger("main")
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s [%(name)s]%(levelname)s:%(message)s')
     devices = connect_to_board()
-    print(devices)
-    print(read_sensors(devices['board']))
+    log.info(f"Found devices: {devices}")
+    log.info(f"Reading sensors")
     camIP = devices['esp32-cam']
     boardIP = devices['board']
+    log.info(read_sensors(boardIP))
     reader = CameraReader(camIP)
     while True:
         img = reader.read()
-        cv2.imshow('img', img)
+        # cv2.imshow('img', img)
         servo = random.uniform(2.5,12.5)
         lucky = random.random() > 0.5
         motorA = random.uniform(*((0,70) if lucky else (30,100)))
         motorB = random.uniform(*((30,100) if lucky else (0,70)))
-        print(f"Set servo to {servo} and motors to {motorA} and {motorB}")
+        log.info(f"servo={servo},motor={motorA},{motorB}")
         control(boardIP, servo, motorA, motorB)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+        # if cv2.waitKey(1) & 0xFF == ord('q'):
+        #     break
 
 if __name__ == "__main__":
     main()
