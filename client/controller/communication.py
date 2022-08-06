3433b45d3b11ae3e55205526dd20a4370666d3dd
kxxt <rsworktech@outlook.com>
2022-08-06 10:04:21 +0800

    feat(client): pyscript to connect to board

---
+++ client/controller/communication.py
@@ -0,0 +1,41 @@
+import requests
+from socket import socket, AF_INET, SOCK_DGRAM
+FRAME_HEADER = "lie-flat device discovery!"
+
+
+def connect_to_board():
+    s = socket(AF_INET, SOCK_DGRAM)
+    s.bind(('', 1234))
+    devices = dict()
+
+    while True:
+        if 'board' in devices:
+            try:
+                response = requests.post(
+                    "http://" + devices['board'] + "/init")
+                if response.status_code == 200:
+                    print("Board initialized!")
+                    break
+            except:
+                continue
+        else:
+            msg, (ip, port) = s.recvfrom(1024)
+            lines = msg.decode().splitlines()
+            if len(lines) < 3:
+                continue
+            itr = iter(lines)
+            if next(itr) != FRAME_HEADER or next(itr) != "Info":
+                # Verify header and verb
+                continue
+            try:
+                for line in itr:
+                    ip, name = line.split("=")
+                    devices[name] = ip
+            except:
+                continue
+    return devices
+
+
+if __name__ == '__main__':
+    devices = connect_to_board()
+    print(devices)
