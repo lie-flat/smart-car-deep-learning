ae0bd937034d887cf31406f944ecd6224c220eff
kxxt <rsworktech@outlook.com>
2022-08-06 17:39:42 +0800

    feat(client): default param for control

--- client/controller/control.py
+++ client/controller/control.py
@@ -1,4 +1,4 @@
 import requests
 
-def control(ip,servo, motorA, motorB):
+def control(ip,servo=7.5, motorA=0, motorB=0):
     requests.post(f"http://{ip}/cmd", {"servo": servo, "motorA": motorA, "motorB": motorB})
