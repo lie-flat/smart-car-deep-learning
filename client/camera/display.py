4964cdde1d0ce521c01b5569657973748e3c76ef
kxxt <rsworktech@outlook.com>
2022-08-19 12:29:54 +0800

    feat: display script

--- client/camera/display.py
+++ client/camera/display.py
@@ -1,47 +1,24 @@
-# Adapted from https://gist.github.com/youjunjer/79e5dad5f47ee5757fcb9d401a95e76b
 import cv2
-import numpy as np
-from urllib.request import urlopen
-import os
-import datetime
-import time
 import sys
+from camera import CameraReader
+from cv import CVReader
 
-# change to your ESP32-CAM ip
-url = f"http://{sys.argv[1]}:81/stream"
-CAMERA_BUFFRER_SIZE = 4096
-stream = urlopen(url)
-bts = b''
-i = 0
 
-while True:
-    try:
-        bts += stream.read(CAMERA_BUFFRER_SIZE)
-        jpghead = bts.find(b'\xff\xd8')
-        jpgend = bts.find(b'\xff\xd9')
-        if jpghead > -1 and jpgend > -1:
-            jpg = bts[jpghead:jpgend+2]
-            bts = bts[jpgend+2:]
-            img = cv2.imdecode(np.frombuffer(
-                jpg, dtype=np.uint8), cv2.IMREAD_UNCHANGED)
-            # img=cv2.flip(img,0) #>0:垂直翻轉, 0:水平翻轉, <0:垂直水平翻轉
-            # h,w=img.shape[:2]
-            #print('影像大小 高:' + str(h) + '寬：' + str(w))
-            # img = cv2.resize(img, (640, 480))
-            cv2.imshow("a", img)
-        k = cv2.waitKey(1)
-    except Exception as e:
-        print("Error:" + str(e))
-        bts = b''
-        stream = urlopen(url)
-        continue
+def main():
+    if len(sys.argv) < 2:
+        print("ERROR: no args!")
+        print("USAGE: display.py pi/<YOUR-ESP32CAM-IP>")
+        return
+    if sys.argv[1] == 'pi':
+        reader = CVReader(480, 320)
+    else:
+        reader = CameraReader(sys.argv[1])
+    while True:
+        img = reader.read()
+        cv2.imshow("img", img)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
-    k = cv2.waitKey(1)
-    # 按a拍照存檔
-    if k & 0xFF == ord('a'):
-        cv2.imwrite(str(i) + ".jpg", img)
-        i = i+1
-    # 按q離開
-    if k & 0xFF == ord('q'):
-        break
-cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
