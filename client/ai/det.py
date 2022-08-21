88d0f31e81602bc455a840d374c820584c167c9e
kxxt <rsworktech@outlook.com>
2022-08-21 16:31:19 +0800

    fix detector

--- client/ai/det.py
+++ client/ai/det.py
@@ -31,11 +31,12 @@ class DetModel:
             (img_info['scale_factor'], )).astype('float32')
         for i in range(len(self.input_names)):
             input_tensor = self.detector.predictor.get_input_handle(self.input_names[i])
+            input_tensor.reshape(inputs[self.input_names[i]].shape)
             input_tensor.copy_from_cpu(inputs[self.input_names[i]])
         self.detector.predictor.run()
-        boxes_tensor = self.predictor.get_output_handle(self.output_names[0])
+        boxes_tensor = self.detector.predictor.get_output_handle(self.output_names[0])
         np_boxes = boxes_tensor.copy_to_cpu()
-        boxes_num = self.predictor.get_output_handle(self.output_names[1])
+        boxes_num = self.detector.predictor.get_output_handle(self.output_names[1])
         np_boxes_num = boxes_num.copy_to_cpu()
         result = dict(boxes=np_boxes, boxes_num=np_boxes_num)
         return result
@@ -44,5 +45,5 @@ class DetModel:
 if __name__ == '__main__':
     img = cv2.imread('../2022-08-06-18-17-42.jpg')
     det = DetModel()
-    result = det.predict('../2022-08-06-18-17-42.jpg' )
+    result = det.predict('../test.jpg' )
     print(result)
