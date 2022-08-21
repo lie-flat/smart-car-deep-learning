91f3354590ebd47d0e657634791b3390b87a1e29
kxxt <rsworktech@outlook.com>
2022-08-21 16:31:01 +0800

    rename signdet -> roadsign

--- client/ai/config.py
+++ client/ai/config.py
@@ -2,6 +2,6 @@ from os import path
 
 fileDir = path.dirname(__file__)
 segModelDir = path.join(fileDir, '..', '..', 'roadseg', 'models')
-detModelDir = path.join(fileDir, '..', '..', 'signdet')
+detModelDir = path.join(fileDir, '..', '..', 'roadsign')
 paddleSegDir = path.join(fileDir, 'PaddleSeg')
 paddleDetDir = path.join(fileDir, 'PaddleDetection')
